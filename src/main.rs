ddda7d0893156736406a196f3274734f38bd913b
Hadiuzzaman <zhhadi50@gmail.com>
2025-08-23 14:40:59 +0600

    feat: using vector to see first element

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+// fn main()
+// {
+//     println!("hadi");
+//     let mut ar: [i32; 4] = [10, 20, 30, 40];
+//     first_element(&mut ar);
+//     println!("{}", ar[0]);
+// }
+
+// fn first_element(ar: &mut [i32])
+// {
+//     ar[0] = 100;
+//     println!("{}", ar[0]);
+// }
+
+
+// now using vector data 
 fn main()
 {
-    println!("hadi");
-    let mut ar: [i32; 4] = [10, 20, 30, 40];
-    first_element(&mut ar);
-    println!("{}", ar[0]);
+    let mut  ar = vec![10, 20, 30, 40];
+    ar.push(50);
+    first_element(&ar);
+    println!("{:?}", ar);
 }
 
-fn first_element(ar: &mut [i32])
+fn first_element(ar: &Vec<i32>)
 {
-    ar[0] = 100;
     println!("{}", ar[0]);
 }
