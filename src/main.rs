cd3666699b924aa6c2d3d3a1b5d0a520efb6475b
Hadiuzzaman <zhhadi50@gmail.com>
2025-08-23 14:15:04 +0600

    fix: emplement static array mutable reference

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 fn main()
 {
     println!("hadi");
-    let ar: [i32; 4] = [10, 20, 30, 40];
-    first_element(ar);
+    let mut ar: [i32; 4] = [10, 20, 30, 40];
+    first_element(&mut ar);
     println!("{}", ar[0]);
 }
 
-fn first_element(ar: [i32; 4])
+fn first_element(ar: &mut [i32])
 {
+    ar[0] = 100;
     println!("{}", ar[0]);
 }
