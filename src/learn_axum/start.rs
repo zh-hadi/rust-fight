a03da6925213a7010e116474f54fea6506074812
Hadiuzzaman <zhhadi50@gmail.com>
2025-11-14 12:27:53 +0600

    using axum here

--- src/learn_axum/start.rs
+++ src/learn_axum/start.rs
@@ -1,10 +1,19 @@
+#[allow(dead_code)]
+
 use tokio::time::Duration;
 use tokio::time::sleep;
+use axum::{routing::get, Router};
 
 pub async fn initialization()
 {
-    println!("hiiiii");
-    sleep(Duration::from_secs(2)).await;
+    let app = Router::new()
+                .route("/", get(home));
+    
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
 
-    println!("hello world");
+pub async fn home()-> &'static str
+{
+    "home page"
 }
